1155f7155c1787d57eda3a4ff6f937cf0ae608d9
Shrey Dikshant <86824571+DaRkAnon1mous@users.noreply.github.com>
2024-11-21 23:43:12 +0530

    Create Code.sql

---
+++ Hackerrank/Weather Observation Station 8/Code.sql
@@ -0,0 +1,7 @@
+/*
+Enter your query here.
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LOWER(SUBSTRING(CITY, 1, 1)) IN ('a', 'e', 'i', 'o', 'u')
+  AND LOWER(SUBSTRING(CITY, -1, 1)) IN ('a', 'e', 'i', 'o', 'u');
